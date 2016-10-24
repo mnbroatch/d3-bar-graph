ce7d167b0a635da006ec120c45c6df53036e0c43
Matthew Broatch <mnbroatch@gmail.com>
2016-10-24 00:08:24 -0700

    animated axes

--- src/d3/d3Chart.js
+++ src/d3/d3Chart.js
@@ -1,7 +1,7 @@
 let d3 = require('d3');
 
 //  so chart has space for axis lines and to breathe a bit
-const margin = {left: 40, bottom: 40, right: 20, top: 20}
+const margin = {left: 60, bottom: 60, right: 20, top: 20}
 const barWidthFactor = .05;
 
 const d3Chart = {};
@@ -18,17 +18,24 @@ d3Chart.create = function (el, state) {
     .attr('height', el.offsetHeight - margin.top - margin.bottom)
     .attr('transform', `translate(${margin.left},-${margin.bottom})`)
 
+
   if (state.data) this.update(el, state);
 };
 
 d3Chart.update = function (el, state) {
   // Re-compute the scales, and render the data points
   let scales = this._scale(el, state);
+  this._drawAxes(el, scales, state);
   this._drawBars(el, scales, state);
 };
 
 d3Chart._scale = function (el, state) {
   let y = d3.scaleLinear()
+    .range([0, el.offsetHeight - margin.top - margin.bottom])
+    .domain([0, this.getMaxHeight(state)])
+
+  //  ugly -- fix later
+  let yReverse = d3.scaleLinear()
     .range([el.offsetHeight - margin.top - margin.bottom, 0])
     .domain([0, this.getMaxHeight(state)])
 
@@ -36,30 +43,42 @@ d3Chart._scale = function (el, state) {
     .range([0, el.offsetWidth - margin.left - margin.right])
     .domain([0, 10])
 
-  return { x, y }
+  return { x, y, yReverse }
 }
 
+d3Chart._drawAxes = function (el, scale, state) {
+  let axisLeft = d3.axisLeft(scale.yReverse)
+  let axisBottom = d3.axisBottom(scale.x)
+  let t = d3.transition()
+    .duration(750);
+
+  const svg = d3.select('.d3-chart__svg')
+
+  if(d3.select('.d3-chart__svg__y-axis').empty()) {
+    svg.append("g")
+      .attr('class', 'd3-chart__svg__y-axis')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .call(axisLeft);
+
+    svg.append("g")
+      .attr('class', 'd3-chart__svg__x-axis')
+      .attr('transform', `translate(${margin.left}, ${el.offsetHeight - margin.bottom})`)
+      .call(axisBottom);
+  }
+
+  let yAxis = d3.select('.d3-chart__svg__y-axis')
+    .transition(t)
+    .call(axisLeft);
+
+  let xAxis = d3.select('.d3-chart__svg__x-axis')
+    .transition(t)
+    .call(axisBottom);
+
+}
 
 d3Chart._drawBars = function (el, scale, state) {
   let t = d3.transition()
     .duration(750);
-
-  let axisLeft = d3.axisLeft(scale.y)
-  let axisBottom = d3.axisBottom(scale.x)
-
-  let svg = d3.select(el).selectAll('.d3-chart__svg');
-
-  svg.append("g")
-    .attr('class', '.d3-chart__svg__y-axis')
-    .attr('transform', `translate(${margin.left}, ${el.offsetHeight - margin.bottom})`)
-    .transition(t)
-    .call(axisBottom);
-
-  svg.append("g")
-    .attr('class', '.d3-chart__svg__x-axis')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
-    .transition(t)
-    .call(axisLeft);
 
   let barWidth = el.offsetWidth * barWidthFactor;
 
